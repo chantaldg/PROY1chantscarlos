87f4095daedf0e6fe2cd7b957d9cd8c247fa8bda
chantaldg <105188742+chantaldg@users.noreply.github.com>
2023-05-05 23:29:15 -0500

    AVANZE

--- app/src/main/java/com/example/proy1_chantscarlos/MainActivity.java
+++ app/src/main/java/com/example/proy1_chantscarlos/MainActivity.java
@@ -1,88 +1,144 @@
 package com.example.proy1_chantscarlos;
 
-import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.SeekBar;
+        import android.widget.TextView;
+        import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText etNombreEstudiante, etUsuarioJuez, etCalificacion1, etCalificacion2, etCalificacion3;
-    private TextView tvPromedio, tvPosicion;
-
-    private static final String[] usuariosJueces = {"juez1", "juez2", "juez3"};
+    private TextView studentNameTextView, score1TextView, score2TextView, score3TextView, resultTextView;
+    private EditText judgeNameEditText;
+    private SeekBar score1SeekBar, score2SeekBar, score3SeekBar;
+    private Button nextButton, submitButton;
+    private int currentStudentIndex = 0;
+    private int currentJudgeIndex = 0;
+    private String[] studentNames = {"CHANTAL DE GRACIA", "CARLOS CAMPBELL", "DIEGO BURGOS", "LUIS CARREYÓ", "MAIKEL DOMÍNGUEZ", "ROMAS LESCURE", "EIVAR MORALES"};
+    private String[] judgeNames = new String[3];
+    private int[][] scores = new int[7][9];
+    private boolean[][] scoresEntered = new boolean[7][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etNombreEstudiante = findViewById(R.id.etNombreEstudiante);
-        etUsuarioJuez = findViewById(R.id.etUsuarioJuez);
-        etCalificacion1 = findViewById(R.id.etCalificacion1);
-        etCalificacion2 = findViewById(R.id.etCalificacion2);
-        etCalificacion3 = findViewById(R.id.etCalificacion3);
-        tvPromedio = findViewById(R.id.tvPromedio);
-        tvPosicion = findViewById(R.id.tvPosicion);
+        studentNameTextView = findViewById(R.id.studentNameTextView);
+        score1TextView = findViewById(R.id.score1TextView);
+        score1SeekBar = findViewById(R.id.score1SeekBar);
+        score2TextView = findViewById(R.id.score2TextView);
+        score2SeekBar = findViewById(R.id.score2SeekBar);
+        score3TextView = findViewById(R.id.score3TextView);
+        score3SeekBar = findViewById(R.id.score3SeekBar);
+        judgeNameEditText = findViewById(R.id.judgeNameEditText);
+        nextButton = findViewById(R.id.nextButton);
+        submitButton = findViewById(R.id.submitButton);
+        resultTextView = findViewById(R.id.resultTextView);
 
-        findViewById(R.id.btnCalcularPromedio).setOnClickListener(view -> {
-            String nombreEstudiante = etNombreEstudiante.getText().toString();
-            String usuarioJuez = etUsuarioJuez.getText().toString();
-            String calificacion1Str = etCalificacion1.getText().toString();
-            String calificacion2Str = etCalificacion2.getText().toString();
-            String calificacion3Str = etCalificacion3.getText().toString();
+        // Initialize UI
+        studentNameTextView.setText(studentNames[0]);
+        score1TextView.setText("Puntuación 1 (Juez 1)");
+        score2TextView.setText("Puntuación 2 (Juez 1)");
+        score3TextView.setText("Puntuación 3 (Juez 1)");
 
-            if (usuarioJuez.isEmpty()) {
-                etUsuarioJuez.setError(getString(R.string.error_juez_requerido));
-                return;
-            }
+        // Set up seek bars
+        score1SeekBar.setMax(9);
+        score2SeekBar.setMax(9);
+        score3SeekBar.setMax(9);
 
-            if (nombreEstudiante.isEmpty()) {
-                etNombreEstudiante.setError(getString(R.string.error_nombre_requerido));
-                return;
-            }
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Move on to next student
+                currentStudentIndex++;
+                // Check if all students have been scored
+                if (currentStudentIndex == 7) {
+                    // Disable next button and enable submit button
+                    nextButton.setEnabled(false);
+                    submitButton.setEnabled(true);  }
+                else {
+                    // Update student name for the next student
+                    studentNameTextView.setText(studentNames[currentStudentIndex]);
+                }
 
-            if (!isValidCalificacion(calificacion1Str) || !isValidCalificacion(calificacion2Str) || !isValidCalificacion(calificacion3Str)) {
-                etCalificacion1.setError(getString(R.string.error_calificacion_invalida));
-                etCalificacion2.setError(getString(R.string.error_calificacion_invalida));
-                etCalificacion3.setError(getString(R.string.error_calificacion_invalida));
-                return;
-            }
-
-            if (!isValidJuez(usuarioJuez)) {
-                etUsuarioJuez.setError(getString(R.string.error_juez_invalido));
-                return;
-            }
-
-            int calificacion1 = Integer.parseInt(calificacion1Str);
-            int calificacion2 = Integer.parseInt(calificacion2Str);
-            int calificacion3 = Integer.parseInt(calificacion3Str);
-
-            if (calificacion1 > 10 || calificacion2 > 10 || calificacion3 > 10) {
-                etCalificacion1.setError(getString(R.string.error_calificacion_invalida));
-                etCalificacion2.setError(getString(R.string.error_calificacion_invalida));
-                etCalificacion3.setError(getString(R.string.error_calificacion_invalida));
-                return;
-            }
-
-            double promedio = (double) (calificacion1 + calificacion2 + calificacion3) / 3;
-            String promedioStr = String.format("%.2f", promedio);
-            String[] registro = new String[] { nombreEstudiante, promedioStr };
-            tvPromedio.setText(getString(R.string.promedio_estudiante, nombreEstudiante, promedioStr));
-
-            String posicion = getPosition(promedio);
-            tvPosicion.setText(getString(R.string.posicion_estudiante, nombreEstudiante, posicion));
-        });
-    }
-
-    private boolean isValidJuez(String usuarioJuez) {
-        for (String usuario : usuariosJueces) {
-            if (usuario.equals(usuarioJuez)) {
-                return true;
+                // Validate judge name
+                String judgeName = judgeNameEditText.getText().toString().trim();
+                if (judgeName.isEmpty()) {
+                    judgeNameEditText.setError("Ingrese el nombre del juez.");
+                    return;
+                }
+                if (Arrays.asList(judgeNames).contains(judgeName)) {
+                    judgeNameEditText.setError("Este juez ya ha evaluado.");
+                    return;
+                }
+                if (currentStudentIndex == 0) {
+                    // Add first judge without checking
+                    judgeNames[currentJudgeIndex] = judgeName;
+                } else {
+                    // Check if the judge has evaluated all previous students
+                    boolean hasEvaluatedAllPrevious = true;
+                    for (int i = 0; i <= currentStudentIndex - 1; i++) {
+                        if (!scoresEntered[i][currentJudgeIndex]) {
+                            hasEvaluatedAllPrevious = false;
+                            break;}
+                    }
+                    if (!hasEvaluatedAllPrevious) {
+                        judgeNameEditText.setError("Este juez debe evaluar al estudiante anterior antes de evaluar a este estudiante.");
+                        return;
+                    } else {
+                        judgeNames[currentJudgeIndex] = judgeName;
+                    }
+                }
+                judgeNameEditText.setText("");
+                // Get scores entered for current student and judge
+                scoresEntered[currentStudentIndex][currentJudgeIndex] = true;
+                if (currentJudgeIndex == 2) {
+                    // Move on to next student if all scores have been entered
+                    boolean allScoresEntered = true;
+                    for (int i = 0; i < 3; i++) {
+                        if (!scoresEntered[currentStudentIndex][i]) {
+                            allScoresEntered = false;
+                            break;
+                        }
+                    }
+                    if (allScoresEntered) {
+                        currentStudentIndex++;
+                        currentJudgeIndex = 0;
+                        if (currentStudentIndex == 7) {
+                            // All students have been scored, calculate results
+                            int[] totalScores = new int[7];
+                            for (int i = 0; i < 7; i++) {
+                                for (int j = 0; j < 9; j++) {
+                                    totalScores[i] += scores[i][j];
+                                }
+                            }
+                            // Determine top three students based on total scores
+                            int[] sortedScores = totalScores.clone();
+                            Arrays.sort(sortedScores);
+                            String[] topThree = new String[3];
+                            for (int i = 0; i < 3; i++) {
+                                int index = Arrays.binarySearch(totalScores, sortedScores[6 - i]);
+                                topThree[i] = studentNames[index];
+                            }
+                            // Display results
+                            String result = "Resultados:\n\n";
+                            for (int i = 0; i < 7; i++) {
+                                result += studentNames[i] + ": " + totalScores[i] + "\n";
+                            }
+                            result += "\nPrimer lugar: " + topThree[0] + "\nSegundo lugar: " + topThree[1] + "\nTercer lugar: " + topThree[2];
+                            resultTextView.setText(result);
+                            submitButton.setEnabled(false);
+                        }
+                    }
+                } else {
+                    currentJudgeIndex++;
+                }
             }
         }
-        return false;
+        );
     }
-
-
+}
