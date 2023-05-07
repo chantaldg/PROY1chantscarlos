f96e52a85921aa715d2173f94627cb831d285f55
chantaldg <105188742+chantaldg@users.noreply.github.com>
2023-05-07 18:46:36 -0500

    casi final ig falta: -validacion de jueces -incorporacion de widget or layout - making it ordered and cute

--- app/src/main/java/com/example/proy1_chantscarlos/MainActivity.java
+++ app/src/main/java/com/example/proy1_chantscarlos/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.proy1_chantscarlos;
 
-        import androidx.appcompat.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.SeekBar;
-        import android.widget.TextView;
-        import java.util.Arrays;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView studentNameTextView, score1TextView, score2TextView, score3TextView, resultTextView;
-    private EditText judgeNameEditText;
-    private SeekBar score1SeekBar, score2SeekBar, score3SeekBar;
+    private EditText judgeNameEditText, score1EditText, score2EditText, score3EditText;
     private Button nextButton, submitButton;
     private int currentStudentIndex = 0;
     private int currentJudgeIndex = 0;
     private String[] studentNames = {"CHANTAL DE GRACIA", "CARLOS CAMPBELL", "DIEGO BURGOS", "LUIS CARREYÓ", "MAIKEL DOMÍNGUEZ", "ROMAS LESCURE", "EIVAR MORALES"};
-    private String[] judgeNames = new String[3];
+    private String[] judgeNames = {"juez1", "juez2", "juez3"};
+
+    private int[][] numScoresPerJudgePerStudent = new int[7][3];
     private int[][] scores = new int[7][9];
     private boolean[][] scoresEntered = new boolean[7][3];
 
@@ -29,11 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
         studentNameTextView = findViewById(R.id.studentNameTextView);
         score1TextView = findViewById(R.id.score1TextView);
-        score1SeekBar = findViewById(R.id.score1SeekBar);
+        score1EditText = findViewById(R.id.score1EditText);
         score2TextView = findViewById(R.id.score2TextView);
-        score2SeekBar = findViewById(R.id.score2SeekBar);
+        score2EditText = findViewById(R.id.score2EditText);
         score3TextView = findViewById(R.id.score3TextView);
-        score3SeekBar = findViewById(R.id.score3SeekBar);
+        score3EditText = findViewById(R.id.score3EditText);
         judgeNameEditText = findViewById(R.id.judgeNameEditText);
         nextButton = findViewById(R.id.nextButton);
         submitButton = findViewById(R.id.submitButton);
@@ -41,63 +44,48 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize UI
         studentNameTextView.setText(studentNames[0]);
-        score1TextView.setText("Puntuación 1 (Juez 1)");
-        score2TextView.setText("Puntuación 2 (Juez 1)");
-        score3TextView.setText("Puntuación 3 (Juez 1)");
-
-        // Set up seek bars
-        score1SeekBar.setMax(9);
-        score2SeekBar.setMax(9);
-        score3SeekBar.setMax(9);
+        score1TextView.setText("Puntuación 1");
+        score2TextView.setText("Puntuación 2");
+        score3TextView.setText("Puntuación 3");
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Move on to next student
-                currentStudentIndex++;
-                // Check if all students have been scored
-                if (currentStudentIndex == 7) {
-                    // Disable next button and enable submit button
-                    nextButton.setEnabled(false);
-                    submitButton.setEnabled(true);  }
-                else {
-                    // Update student name for the next student
-                    studentNameTextView.setText(studentNames[currentStudentIndex]);
+                // Validate judge name
+                String judgeName = judgeNameEditText.getText().toString();
+                if (!Arrays.asList(judgeNames).contains(judgeName)) {
+                    Toast.makeText(MainActivity.this, "Nombre del juez inválido", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
-                // Validate judge name
-                String judgeName = judgeNameEditText.getText().toString().trim();
-                if (judgeName.isEmpty()) {
-                    judgeNameEditText.setError("Ingrese el nombre del juez.");
+                // Get scores entered for current student
+                int score1 = Integer.parseInt(score1EditText.getText().toString());
+                int score2 = Integer.parseInt(score2EditText.getText().toString());
+                int score3 = Integer.parseInt(score3EditText.getText().toString());
+
+                // Check if scores are within valid range
+                if (score1 < 1 || score1 > 10 || score2 < 1 || score2 > 10 || score3 < 1 || score3 > 10) {
+                    Toast.makeText(MainActivity.this, "Puntuación debe estar entre 1 y 10", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (Arrays.asList(judgeNames).contains(judgeName)) {
-                    judgeNameEditText.setError("Este juez ya ha evaluado.");
-                    return;
-                }
-                if (currentStudentIndex == 0) {
-                    // Add first judge without checking
-                    judgeNames[currentJudgeIndex] = judgeName;
-                } else {
-                    // Check if the judge has evaluated all previous students
-                    boolean hasEvaluatedAllPrevious = true;
-                    for (int i = 0; i <= currentStudentIndex - 1; i++) {
-                        if (!scoresEntered[i][currentJudgeIndex]) {
-                            hasEvaluatedAllPrevious = false;
-                            break;}
-                    }
-                    if (!hasEvaluatedAllPrevious) {
-                        judgeNameEditText.setError("Este juez debe evaluar al estudiante anterior antes de evaluar a este estudiante.");
+
+                /* Check if current judge has already scored the current student
+                for (int i = 0; i < 3; i++) {
+                    if (scoresEntered[currentStudentIndex][i] && judgeNames[i].equals(judgeName)) {
+                        Toast.makeText(MainActivity.this, "Este juez ya ha calificado a este estudiante", Toast.LENGTH_SHORT).show();
                         return;
-                    } else {
-                        judgeNames[currentJudgeIndex] = judgeName;
                     }
                 }
-                judgeNameEditText.setText("");
-                // Get scores entered for current student and judge
+*/
+                // Update scores for current student and judge
+                scores[currentStudentIndex][currentJudgeIndex * 3] = score1;
+                scores[currentStudentIndex][currentJudgeIndex * 3 + 1] = score2;
+                scores[currentStudentIndex][currentJudgeIndex * 3 + 2] = score3;
                 scoresEntered[currentStudentIndex][currentJudgeIndex] = true;
+
+                // Move on to next judge or student
                 if (currentJudgeIndex == 2) {
-                    // Move on to next student if all scores have been entered
+                    // Move on to next student if all scores have been entered for current student
                     boolean allScoresEntered = true;
                     for (int i = 0; i < 3; i++) {
                         if (!scoresEntered[currentStudentIndex][i]) {
@@ -108,37 +96,94 @@ public class MainActivity extends AppCompatActivity {
                     if (allScoresEntered) {
                         currentStudentIndex++;
                         currentJudgeIndex = 0;
-                        if (currentStudentIndex == 7) {
-                            // All students have been scored, calculate results
-                            int[] totalScores = new int[7];
-                            for (int i = 0; i < 7; i++) {
-                                for (int j = 0; j < 9; j++) {
-                                    totalScores[i] += scores[i][j];
-                                }
-                            }
-                            // Determine top three students based on total scores
-                            int[] sortedScores = totalScores.clone();
-                            Arrays.sort(sortedScores);
-                            String[] topThree = new String[3];
-                            for (int i = 0; i < 3; i++) {
-                                int index = Arrays.binarySearch(totalScores, sortedScores[6 - i]);
-                                topThree[i] = studentNames[index];
-                            }
-                            // Display results
-                            String result = "Resultados:\n\n";
-                            for (int i = 0; i < 7; i++) {
-                                result += studentNames[i] + ": " + totalScores[i] + "\n";
-                            }
-                            result += "\nPrimer lugar: " + topThree[0] + "\nSegundo lugar: " + topThree[1] + "\nTercer lugar: " + topThree[2];
-                            resultTextView.setText(result);
-                            submitButton.setEnabled(false);
-                        }
                     }
                 } else {
+                    // Move on to next judge
                     currentJudgeIndex++;
                 }
+
+                // Check if all students have been scored
+                if (currentStudentIndex == 7) {
+                    // Disable next button and enable submit button
+                    nextButton.setEnabled(false);
+                    submitButton.setEnabled(true);
+                } else {
+                    // Update student name and scores for the next student
+                    studentNameTextView.setText(studentNames[currentStudentIndex]);
+                    score1EditText.setText("");
+                    score2EditText.setText("");
+                    score3EditText.setText("");
+                }
             }
-        }
-        );
-    }
-}
+        });
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Calculate results
+                float[] averageScores = new float[7];
+                for (int i = 0; i < 7; i++) {
+                    float totalScore = 0;
+                    int scoresEnteredCount = 0;
+                    for (int j = 0; j < 9; j++) {
+                        if (scoresEntered[i][j / 3]) {
+                            totalScore += scores[i][j];
+                            scoresEnteredCount++;
+                        }
+                    }
+                    if (scoresEnteredCount > 0) {
+                        averageScores[i] = totalScore / scoresEnteredCount;
+                    }
+                }
+
+                // Determine top three students based on average scores
+                String[] topThree = new String[3];
+                float[] sortedScores = averageScores.clone();
+                Arrays.sort(sortedScores);
+                int topThreeCount = 0;
+                for (int i = 0; i < 7; i++) {
+                    if (averageScores[i] > 0) {
+                        if (topThreeCount < 3) {
+                            topThree[topThreeCount++] = studentNames[i];
+                        } else {
+                            for (int j = 0; j < 3; j++) {
+                                if (averageScores[i] > sortedScores[6 - j]) {
+                                    for (int k = 2; k > j; k--) {
+                                        topThree[k] = topThree[k - 1];
+                                    }
+                                    topThree[j] = studentNames[i];
+                                    sortedScores[6 - j] = averageScores[i];
+                                    break;
+                                }
+                                if (averageScores[i] == sortedScores[6 - j]) {
+                                    // There is a tie
+                                    String tieMsg = "Empate entre ";
+                                    int tieCount = 1;
+                                    for (int k = j; k < 3; k++) {
+                                        if (averageScores[i] == sortedScores[6 - k]) {
+                                            if (tieCount > 1) {
+                                                tieMsg += " y ";
+                                            }
+                                            tieMsg += topThree[k];
+                                            tieCount++;
+                                        }
+                                    }
+                                    tieMsg += " en el puesto " + (j + 1) + ".";
+                                    resultTextView.setText(tieMsg);
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // Display results for top three students
+                String result = "Resultados:\n\n";
+                for (int i = 0; i < topThreeCount; i++) {
+                    result += topThree[i] + ": " + String.format("%.2f", sortedScores[6 - i]) + "\n";
+                }
+                resultTextView.setText(result);
+
+                // Disable submit button
+                submitButton.setEnabled(false);
+            }
+        });}}
